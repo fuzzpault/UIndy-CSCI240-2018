96e98ae5a3c24c391226ff24407fa4c801eb5de3
Paul Talaga <talagap@uindy.edu>
2018-10-17 17:33:46 +0000

    Lab08 Starter

---
+++ W08-Tree2/Lab08Starter/BST.h
@@ -0,0 +1,90 @@
+#ifndef BST_H
+#define BST_H
+/* Name: // Fill me in
+   Date: Oct 16, 2018
+   Desc: Binary search tree class to store strings
+*/
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <stdexcept>
+#include <cassert>
+
+using namespace std;
+
+// Definition of a node 
+struct Node_t{
+  string data;
+  Node_t* left;
+  Node_t* right;
+};
+
+class BST{
+    private:
+      // Fill me in
+  
+    public:
+    
+    BST(){
+      // Fill me in
+    }
+  
+    ~BST(){
+        // Fill me in
+    }
+  
+    BST(const BST &other){
+      
+    }
+  
+    // Insert word into the BST
+    void insert(const string &word){
+        // Fill me in
+    }
+  
+    // Number of nodes in the BST
+    unsigned size()const{
+        // Fill me in
+        return 0;
+    }
+    
+    // The height of the tree (number of edges on longest path)
+    unsigned height()const{
+        // Fill me in
+        return 0;
+    }
+    
+    // Preorder traversal of the tree
+    // Just separate elements with a space, no []
+    string preOrderTraversal()const{
+        // Fill me in
+    }
+    
+    // Postorder traversal of the tree
+    // Just separate elements with a space, no []
+    string inOrderTraversal()const{
+        // Fill me in
+    }
+    
+    // Returns true if that value is stored, false otherwise
+    bool find(const string &value)const{
+        // Fill me in
+        return false;
+    }
+    
+    // Returns true if the tree is balanced, false otherwise
+    // For all nodes, the number of nodes to the right and to the left
+    // differ by at most 1.
+    bool isBalanced()const{
+        // Fill me in
+        return false;
+    }
+    
+    private:
+    
+    // Fill me in
+    
+};
+
+#endif
+
---
+++ W08-Tree2/Lab08Starter/BST_test.cpp
@@ -0,0 +1,20 @@
+/* Name: < Fill me in>
+   Date: < Fill me in>
+   Desc: Unit tests for the BST classes
+*/
+#include "gtest/gtest.h"
+
+#include "BST.h"
+
+
+
+/*  // Code to remind you what unit tests look like.
+TEST(size, 0){
+  LIST a;
+  EXPECT_EQ(a.size(), 0);
+}
+*/
+
+
+
+
